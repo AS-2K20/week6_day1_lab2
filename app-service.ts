90d8714ea056e727f00978df880490e024e67475
AS-2K20 <amudhan.psgcas@gmail.com>
2020-07-28 09:59:37 +0530

    string operations in TS

--- app-service.ts
+++ app-service.ts
@@ -1,3 +1,11 @@
-export interface ArrayService {
+// export interface ArrayService {
 
+// }
+
+export interface StringManipulationService {
+    arrayFindMultiples(myArray : any) : Array<any>;
+    arraySeparate(myArray : any) : Array<any>;
+    arraySplit(myString : string) : Array<String>;
+    arraySort(myArray : any) : Array<any>;
+    arrayReplace(myArray : any) : Array<any>;
 }
--- main.ts
+++ main.ts
@@ -1,36 +1,93 @@
-import { ArrayService } from "./app-service";
+// import { ArrayService } from "./app-service";
 
-export class ArrayComponent implements ArrayService {
+// export class ArrayComponent implements ArrayService {
 
-    constructor() {}
+//     constructor() {}
 
-    public arrayMultiply(myArray: number[]) : Array<Number> {
+//     public arrayMultiply(myArray: number[]) : void Array<Number>() {
       
-    }
+//     }
 
-    public arraySeparate(myArray: any) : Array<string>{
+//     public arraySeparate(myArray: any) : Array<string>{
         
-    }
+//     }
 
-    public arraySplit(str:string) : Array<number>{
+//     public arraySplit(str:string) : Array<number>{
 
-    }
+//     }
 
-    public arraySort(myArray:any):Array<string>{
+//     public arraySort(myArray:any):Array<string>{
       
-    }
+//     }
 
-    public arrayReplace(myArray:any):Array<number>{
+//     public arrayReplace(myArray:any):Array<number>{
        
+// }
+
+// let myArray: number[] = [34, 45, 60, 23, 13, 25, 70];
+// let array = new ArrayComponent();
+
+// console.log(array.arrayMultiply(myArray));
+// console.log(array.arraySeparate(myArray));
+// console.log(array.arraySort(myArray));
+// console.log(array.arrayReplace(myArray));
+
+import {StringManipulationService} from "./app-service";
+
+class ArrayManipulations  implements StringManipulationService{
+    arrayFindMultiples(myArray : any) : Array<any>{
+        let i :number;
+        let resultArray: Array<any> = [];
+        for (i = 0; i < myArray.length; i++){
+            if ((myArray[i]%5==0) && myArray[i]%10==0 ) {
+                resultArray.push(myArray[i]);
+             }
+        }
+        return resultArray;
+    }
+    arraySeparate(myArray : any) : Array<any>{
+        let i :number;
+        let resultStringArray: Array<any> = [];
+        for (i = 0; i < myArray.length; i++) { 
+        if (typeof (myArray[i]) == "string") {
+            resultStringArray.push(myArray[i]);
+        }
+    }
+    return resultStringArray;
+    }
+    arraySplit(myString : string) : Array<String>{
+        let i :number;
+        let numbersArray: Array<number> = [];
+        let resultOddNumbersArray: Array<any> = [];
+        for (i = 0; i < myString.length; i++) { 
+            if (typeof (myString[i]) == "number") {
+                numbersArray.push(parseInt(myString[i]));
+            }    
+        }
+        for (i = 0; i < numbersArray.length; i++) { 
+            if (numbersArray[i] % 2 != 0) { 
+                resultOddNumbersArray.push(numbersArray[i]);
+            }
+        }
+            return resultOddNumbersArray;
+    }
+    arraySort(myArray : any) : Array<any>{
+        return myArray.sort();
+    }
+    arrayReplace(myArray : any) : Array<any>{
+        let i :number;
+        let numbersArray: Array<number> = [];
+       
+        for (i = 0; i < myArray.length; i++) { 
+            if (typeof (myArray[i]) == "number") {
+                numbersArray.push(parseInt(myArray[i]));
+            }    
+        }
+        for (i = 0; i < numbersArray.length; i++) { 
+            if (numbersArray[i] % 3== 0) { 
+                numbersArray[i]=5;
+            }
+        }
+        return numbersArray;
+    }
 }
-
-let myArray: number[] = [34, 45, 60, 23, 13, 25, 70];
-let array = new ArrayComponent();
-
-console.log(array.arrayMultiply(myArray));
-console.log(array.arraySeparate(myArray));
-console.log(array.arraySort(myArray));
-console.log(array.arrayReplace(myArray));
-
-
-
